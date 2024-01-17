0d723bec1cfb6e2bea5921a4389cce6bcf992ac2
ivanLecomte <143179458+ivanLecomte@users.noreply.github.com>
2024-01-17 21:35:54 +0100

    Add files via upload

--- register.py
+++ register.py
@@ -25,7 +25,7 @@ def insert_new_acc_data(username, password):
                 print(f"L'utilisateur avec le nom d'utilisateur '{username}' existe déjà.")
                 return
             
-            #If the code arrives here it means user count was 0, so no user, then we can insert the data into the table, but first we hash the password because we do not wanter raw password in database
+            #If the code arrives here it means user count was 0, so no user in the database with the username given, then we can insert the data into the table, but first we hash the password because we do not wante raw password in database
             hashed_password = hashlib.sha256(password.encode()).hexdigest()
             query = f"INSERT INTO players (pseudo, password) VALUES ('{username}','{hashed_password}')"
             cursor.execute(query)
@@ -35,51 +35,47 @@ def insert_new_acc_data(username, password):
 
     except Error as e:
         print(f"Erreur lors de l'insertion des données du nouveau compte : {e}")
-
+    #Whatever happended the connections will be closed
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
             print("Connexion à la base de données fermée")
 
-
+#This function compare user infos with the ones arleady in the database
 def log_user(username, password):
         try:
             connection = database.connect_to_database()
             if connection:
                 cursor = connection.cursor(dictionary=True)
 
-                # Récupérer le mot de passe haché de l'utilisateur dans la base de données
+                #Select in the database the username that has been asked
                 cursor.execute(f"SELECT id, pseudo, password FROM players WHERE pseudo = '{username}'")
                 user_data = cursor.fetchone()
-
+                #Here to know if user_data is True, then it means that the user has been found
                 if user_data:
                     user_id = user_data['id']
                     stored_password = user_data['password']
 
-                    # Hasher le mot de passe fourni par l'utilisateur pour le comparer avec celui stocké
+                    #Transform the raw password in the crypted one
                     hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
+                    #Here is the security, it checks if the password given is the same as the one in the database, to identify the user
                     if hashed_password == stored_password:
-                        print(f"Utilisateur '{username}' connecté avec succès. ID: {user_id}")
+                        print(f"Utilisateur '{username}' connecté avec succès. ID: {user_id}")#if it was the same the user can be logged
                         return True, user_id
                     else:
-                        print(f"Mot de passe incorrect pour l'utilisateur '{username}'")
+                        print(f"Mot de passe incorrect pour l'utilisateur '{username}'")#if it was wrong, access denied  
                         return False
 
-                else:
+                else: #Here in the case if the previous if got False, it means the user hasn't been found in the database
                     print(f"L'utilisateur '{username}' n'existe pas dans la base de données.")
                     return False
-
+        #Here in the case if the connection with the database is not established
         except Error as e:
             print(f"Erreur lors de la connexion de l'utilisateur : {e}")
-
+        #Whatever happended the connections will be closed
         finally:
             if connection.is_connected():
                 cursor.close()
                 connection.close()
                 print("Connexion à la base de données fermée")
-
-
-#insert_new_acc_data("ivan", "1234")
-#log_user("ivan", "1234")
